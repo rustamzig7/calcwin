81a34f712d67b9f886dae07b1471328b6a952c1a
daniloffszz <daniloff102rus@yndex.ru>
2024-01-27 02:18:11 +0500

    Изображения и хранилище массива

---
+++ src/main/java/JavaWindows/Pair.java
@@ -0,0 +1,12 @@
+package JavaWindows;
+import java.util.Arrays;
+
+public class Pair {
+	int first;
+    int second;
+
+    Pair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
