22eb8cd125adbf2c023a9c1934bc8670e794f9a6
rainem41 <rainem41@gmail.com>
2024-01-27 03:00:58 +0500

    Создание теста

--- src/test/java/JavaWindows/WindowCalculatorTest.java
+++ src/test/java/JavaWindows/WindowCalculatorTest.java
@@ -13,15 +13,24 @@ public class WindowCalculatorTest {
     public void testGetResultTypeInt() {
         WindowCalculator calculator = new WindowCalculator();
 
-        // Test case 1
         int resultTypeInt1 = calculator.getResultTypeInt("Односекционное");
         assertEquals(1, resultTypeInt1);
 
-        // Test case 2
         int resultTypeInt2 = calculator.getResultTypeInt("Трехсекционные");
         assertEquals(3, resultTypeInt2);
     }
 
+    @Test
+    public void testGetHeightAndWidthPrice() {
+        WindowCalculator calculator = new WindowCalculator();
 
+        Pair result1 = calculator.getHeightAndWidthPrice("Эконом");
+        assertEquals(1, result1.first);
+        assertEquals(2, result1.second);
+
+        Pair result2 = calculator.getHeightAndWidthPrice("Премиум");
+        assertEquals(3, result2.first);
+        assertEquals(3, result2.second);
+    }
 
 }
